85a8891705de3b9092fb33f862617df66c8fcec2
hanmy <hanmy2023@stu.pku.edu.cn>
2025-07-29 11:17:34 +0800

    1

--- planeAero_case.py
+++ planeAero_case.py
@@ -20,29 +20,34 @@ with open("wake_test_fuselage_wake.inp","r") as filee:
             labels = [int(t)-1 if t not in ["2071","2158"] else 1259 for t in line[1:].split()]
             shells_ids["body"].append(len(shells))
             shells.append(labels[:4])
+        '''
         if line[0] == "1" and line[1] == "0":
             labels = [int(t)-1 if t not in ["2071","2158"] else 1259 for t in line[2:].split()]
             shells_ids["wake"].append(len(shells))
-            shells.append(labels[:4])
+            shells.append(labels[:4])'''
         if line[0] == "2":
             labels = [int(t)-1 if t not in ["2071","2158"] else 1259 for t in line[1:].split()]
             shells_ids["body"].append(len(shells))
             shells.append(labels[:3])
 
 mesh = PanelAeroMesh(nodes, shells, shells_ids)
-'''
+
 V_fs = Vector((1,0,0))
 V_fs_norm = V_fs.norm()
-mu = []                         ## 先在集群上计算出mu的值再回来计算Cp
+mu = []         ## 先在集群上计算出mu的值再回来计算Cp
+Cp = []                  
 with open("mu.txt","r") as file:
     for line in file:
         mu.append(float(line))
+with open("Cp.txt","r") as file:
+    for line in file:
+        Cp.append(float(line))
 print(len(mu))
 print(len(shells))
 mu = mu[:len(shells)]
 for j,m in enumerate(mu):
     mesh.panels[j].mu = m
-
+L = 0
 for i,panel in enumerate(mesh.panels):
             
     panel_neighbours = mesh.give_neighbours3(panel,3)
@@ -50,10 +55,14 @@ for i,panel in enumerate(mesh.panels):
     panel.Velocity = panel_velocity3(panel, panel_neighbours, V_fs, rcond=1e-2)
             
     panel.Cp = 1 - (panel.Velocity.norm()/V_fs_norm)**2
-'''
+    L -= panel.Cp * panel.area * (panel.n * Vector((0,0,1)))
+
 Cp = [panel.Cp for panel in mesh.panels]
+'''
 for i in mesh.TrailingEdge["suction side"]:
     Cp[i] = 1
 for i in mesh.TrailingEdge["pressure side"]:
     Cp[i] = -1
+'''
 plot_savedCp_SurfaceContours(mesh.panels, Cp, elevation=20, azimuth=45)
+print(L)
